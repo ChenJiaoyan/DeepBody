29c5d35fec55d75ab598184fced5ffb969bf176b
Jiaoyan Chen <chen00217@gmail.com>
2016-12-13 19:20:42 +0100

    Rejected commit: Default

--- src/main/java/org/deepbody/ImagePipline.java
+++ src/main/java/org/deepbody/ImagePipline.java
@@ -1,7 +1,15 @@
 package org.deepbody;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
 /**
  * Created by john on 13.12.16.
  */
 public class ImagePipline {
+    public static void main(String args[]){
+        INDArray arr1 = Nd4j.create(new float[]{1,2,3,4},new int[]{2,2});
+        System.out.println("arr1:");
+        System.out.println(arr1);
+    }
 }
