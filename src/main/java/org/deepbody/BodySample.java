7bf3913d05d4313623b7ad4ce99fe9e129ad53cb
Jiaoyan Chen <chen00217@gmail.com>
2016-12-19 17:08:14 +0100

    architecture

---
+++ src/main/java/org/deepbody/BodySample.java
@@ -0,0 +1,10 @@
+package org.deepbody;
+
+/**
+ * Created by john on 19.12.16.
+ */
+public class BodySample {
+    public static int height = 100;
+    public static int width = 100;
+    public static int channels = 3;
+}
