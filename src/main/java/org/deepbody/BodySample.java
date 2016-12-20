afbf3b4f6c5be28ca1712f81f407bc77154b7bb0
John <John@John-Pro.local>
2016-12-20 09:01:28 +0100

    Merge branch 'master' of https://github.com/ChenJiaoyan/DeepBody
    # Conflicts:
    #	logs/application.log

--- src/main/java/org/deepbody/BodySample.java
+++ src/main/java/org/deepbody/BodySample.java
@@ -1,10 +1,13 @@
 package org.deepbody;
 
+import javax.imageio.ImageIO;
+
 /**
  * Created by john on 19.12.16.
  */
 public class BodySample {
-    public static int height = 100;
-    public static int width = 100;
+    public static int height = 50;
+    public static int width = 50;
     public static int channels = 3;
+
 }
