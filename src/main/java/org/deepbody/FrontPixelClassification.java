38386aaa386d98c52829dcc189ec91efdc2c3765
Jiaoyan Chen <chen00217@gmail.com>
2016-12-20 17:23:49 +0100

    architecture

--- src/main/java/org/deepbody/FrontPixelClassification.java
+++ src/main/java/org/deepbody/FrontPixelClassification.java
@@ -1,14 +1,15 @@
 package org.deepbody;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Created by john on 14.12.16.
  * Classify the image pixel into body parts: head (A), tors (B), arms (C), legs (D), and others (E)
  */
 public class FrontPixelClassification {
-    protected static final Logger log = LoggerFactory.getLogger(FrontPixelClassification.class);
+    protected static int height = 64;
+    protected static int weight = 64;
+    protected static int channels = 3;
+    protected static int outputNum = 11;
     public static void main(String args []){
 
     }
