0b48fb3da4199a4c79d388f2a14a36a2ca11ffad
John <John@vpn233a.rzuser.uni-heidelberg.de>
2017-01-05 17:16:16 +0100

    FrontPixelClassification

--- src/main/java/org/utils/DrawDotPanel.java
+++ src/main/java/org/utils/DrawDotPanel.java
@@ -1,8 +1,11 @@
 package org.utils;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.ArrayList;
 
 /**
  * Created by lisa on 1/2/17.
@@ -47,4 +50,57 @@ public class DrawDotPanel extends JPanel {
     public Dimension getPreferredSize() {
         return dims;
     }
+
+    public static void showResult(File predict_f, ArrayList<int []> locations) {
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(predict_f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DrawDotPanel panel = new DrawDotPanel(image);
+        frame.add(panel);
+
+        /**
+         *  dot[1] is the x position, dot[0] is the y position
+         */
+        for (int[] dot : locations) {
+            panel.drawDot(dot[1], dot[0]);
+        }
+
+        panel.repaint();
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String args[]) throws IOException {
+        String location_file = "locations_front";
+        File location_f = new File(System.getProperty("user.dir"), "src/main/resources/Body/" + location_file);
+        InputStreamReader reader = new InputStreamReader(new FileInputStream(location_f));
+        BufferedReader br = new BufferedReader(reader);
+        ArrayList<int []> locations = new ArrayList<>();
+        String line = br.readLine();
+        String img_name = "207034429.jpg";
+        File predict_f = new File(System.getProperty("user.dir"),
+                "src/main/resources/Body/Prediction/Front/" + img_name);
+
+        while(line!=null){
+            line = br.readLine();
+            if(line.startsWith(img_name)){
+                String [] tmp = line.split(";");
+                for(int i=0;i<tmp.length;i++){
+                    String [] tmp2 = tmp[i].split(",");
+                    int [] loc = {Integer.parseInt(tmp2[0]),Integer.parseInt(tmp2[1])};
+                    locations.add(loc);
+                }
+            }
+        }
+        showResult(predict_f,locations);
+    }
+
 }
